1b823f9275d9f257879515067c478662c9114205
sutezo <sutezo666@gmail.com>
2025-11-23 16:50:30 +0900

    Renew

--- examples/svelte-components/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.ET3ey3IB.js","_app/immutable/chunks/BG9Hx8eb.js","_app/immutable/chunks/Z1kvhb-i.js","_app/immutable/chunks/DMYeqAF0.js"];
-export const stylesheets = [];
-export const fonts = [];
--- examples/svelte-components/.svelte-kit/output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/documentation/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.me9eXOY1.js","_app/immutable/chunks/BG9Hx8eb.js"];
-export const stylesheets = [];
-export const fonts = [];
